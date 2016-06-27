096642cb2dd9be866cb5950a1052f4a170a80880
derickarzu <arzuderick@yahoo.com>
2016-06-26 21:27:10 -0600

    Finalizado el proyecto
    :) so happy

--- Examen/manejodecuentas.cpp
+++ Examen/manejodecuentas.cpp
@@ -24,7 +24,7 @@ void ManejodeCuentas::addCuentaPersonal(CuentaPersonal cp){
         }
         //int cuenta = cp.getCuenta();
         //double saldo = cp.getSaldo();
-        out.seekp(0,ios::end);
+        out.seekp(0);
         out.write((char*)&cp.numeroCuenta,4);
         out.write(cp.getNombre().c_str(),20);
         out.write(cp.getNombreBanco().c_str(),20);
@@ -39,7 +39,7 @@ void ManejodeCuentas::addCuentaHijos(AhorroH ahorros){
         {
             out.open(nombre_archivo.c_str());
         }
-        out.seekp(0,ios::end);
+        out.seekp(0);
         out.write((char*)&ahorros.numeroCuenta,4);
         out.write(ahorros.getNombre().c_str(),20);
         out.write(ahorros.getNombreBanco().c_str(),20);
@@ -61,7 +61,12 @@ CuentaPersonal *ManejodeCuentas::leerCuentaPersonal(){
             in.read((char*)&saldo,sizeof(saldo));
 
         in.close();
-        return new CuentaPersonal(cuenta,nombreBanco,nombre,saldo);
+        if(saldo!=0){
+            return new CuentaPersonal(cuenta,nombreBanco,nombre,saldo);
+        }
+        else{
+            return new CuentaPersonal(0,"","",0);
+        }
 }
 
 AhorroH *ManejodeCuentas::leerCuentaHijo(){
@@ -78,11 +83,72 @@ AhorroH *ManejodeCuentas::leerCuentaHijo(){
             in.read((char*)&saldo,sizeof(saldo));
 
         in.close();
-        return new AhorroH(cuenta,nombreBanco,nombre,saldo);
+        if(saldo!=0){
+            return new AhorroH(cuenta,nombreBanco,nombre,saldo);
+        }
+        else{
+            return new AhorroH(0,"","",0);
+        }
 }
 
 
 ManejodeCuentas::~ManejodeCuentas()
 {
     delete ui;
+}
+
+void ManejodeCuentas::on_depositar_clicked()
+{
+    double saldo = (leerCuentaPersonal())->getSaldo();
+    QString montoDeposito = ui->montofield->text();
+    double monto = montoDeposito.split(" ")[0].toDouble();
+    QString saldoActual = QString::number(saldo+monto);
+    ui->saldoactualpersonal->setText(saldoActual);
+    int cuenta = (leerCuentaPersonal())->getCuenta();
+    string nombre = (leerCuentaPersonal())->getNombre();
+    string nombreBanco = (leerCuentaPersonal())->getNombreBanco();
+    CuentaPersonal cuentaActualizada(cuenta,nombreBanco,nombre,saldo+monto);
+    addCuentaPersonal(cuentaActualizada);
+    ui->depositar->setEnabled(false);
+}
+
+void ManejodeCuentas::on_depositar2_clicked()
+{
+    double saldo = (leerCuentaHijo())->getSaldo();
+    QString montoDeposito = ui->montofield->text();
+    double monto = montoDeposito.split(" ")[0].toDouble();
+    QString saldoActual = QString::number(saldo+monto);
+    ui->saldoactualhijo->setText(saldoActual);
+    int cuenta = (leerCuentaHijo())->getCuenta();
+    string nombre = (leerCuentaHijo())->getNombre();
+    string nombreBanco = (leerCuentaHijo())->getNombreBanco();
+    AhorroH cuentaActualizada(cuenta,nombreBanco,nombre,saldo+monto);
+    addCuentaHijos(cuentaActualizada);
+    double saldo2 = (leerCuentaPersonal())->getSaldo();
+    QString saldoActual2 = QString::number(saldo2-monto);
+    ui->saldoactualpersonal->setText(saldoActual2);
+
+    //actualizar en archivo
+    int cuenta2 = (leerCuentaPersonal())->getCuenta();
+    string nombre2 = (leerCuentaPersonal())->getNombre();
+    string nombreBanco2 = (leerCuentaPersonal())->getNombreBanco();
+    CuentaPersonal cuentaActualizada2(cuenta2,nombreBanco2,nombre2,saldo2-monto);
+    addCuentaPersonal(cuentaActualizada2);
+    ui->depositar2->setEnabled(false);
+    ui->depositar->setEnabled(false);
+}
+
+void ManejodeCuentas::on_retirar_clicked()
+{
+    double saldo = (leerCuentaPersonal())->getSaldo();
+    QString montoDeposito = ui->retmontofield->text();
+    double monto = montoDeposito.split(" ")[0].toDouble();
+    QString saldoActual = QString::number(saldo-monto);
+    ui->saldoactualpersonal->setText(saldoActual);
+    int cuenta = (leerCuentaPersonal())->getCuenta();
+    string nombre = (leerCuentaPersonal())->getNombre();
+    string nombreBanco = (leerCuentaPersonal())->getNombreBanco();
+    CuentaPersonal cuentaActualizada(cuenta,nombreBanco,nombre,saldo-monto);
+    addCuentaPersonal(cuentaActualizada);
+    ui->retirar->setEnabled(false);
 }
